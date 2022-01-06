b348e0b78318cea7cb9526cce9e83a2c2e7c46e4
trantuyen082001 <trantuyen082001@gmail.com>
2022-01-06 05:29:12 -1200

    m

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,24 @@
+import bcryptjs from "bcryptjs"
+
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: xxxx,
+        password: bcryptjs.hashSync('123456',10),
         isAdmin: true
     },
     {
         name: 'Tuyen Tran',
         email: 'tuyen@example.com',
-        password: xxxx,
+        password: bcryptjs.hashSync('123456',10),
+
     },
     {
         name: 'Bin Tran',
         email: 'bin@example.com',
-        password: xxxx,
+        password: bcryptjs.hashSync('123456',10),
+
     }
 ]
+
+export default users;
