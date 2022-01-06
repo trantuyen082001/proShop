233543b0627ec975c9ee6ed21ea4c2b3264af7e7
trantuyen082001 <trantuyen082001@gmail.com>
2022-01-06 04:29:41 -1200

    add models and mongooseDB

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: 'Tuyen Tran',
+        email: 'tuyen@example.com',
+        password: xxxx,
+    },
+    {
+        name: 'Bin Tran',
+        email: 'bin@example.com',
+        password: xxxx,
+    }
+]
